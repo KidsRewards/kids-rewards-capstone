e117bf83cae91b1604b13b67c01bb38bb9d91312
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-03-08 14:09:34 -0600

    Added constructors, getters, and setters for Task.java

--- src/main/java/com/codeup/kidsrewardscapstone/models/Task.java
+++ src/main/java/com/codeup/kidsrewardscapstone/models/Task.java
@@ -32,4 +32,82 @@ public class Task {
     @OneToOne
     @JoinColumn (name = "status_id")
     private Status status;
+
+    public Task() {
+    }
+
+    public Task(long id, String title, String description, long points, long due_date, Admin admin, User user, Status status) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.points = points;
+        this.due_date = due_date;
+        this.admin = admin;
+        this.user = user;
+        this.status = status;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getPoints() {
+        return points;
+    }
+
+    public void setPoints(long points) {
+        this.points = points;
+    }
+
+    public long getDue_date() {
+        return due_date;
+    }
+
+    public void setDue_date(long due_date) {
+        this.due_date = due_date;
+    }
+
+    public Admin getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Admin admin) {
+        this.admin = admin;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
