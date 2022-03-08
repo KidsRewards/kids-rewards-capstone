985480884acd9cd95a02df25eae0acf974c7cc6e
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-03-08 12:00:54 -0600

    set up database

---
+++ src/main/resources/static/static/sql/seeder.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE IF NOT EXISTS kidsrewards_db;
+
+CREATE USER IF NOT EXISTS kidsrewards_user@localhost IDENTIFIED BY 'p@$$w0rd';
+GRANT ALL ON kidsrewards_db.* TO kidsrewards_user@localhost;
+
+USE kidsrewards_db;
