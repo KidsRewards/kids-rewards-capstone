d507e38bec96d55afdf4485919fb6ae5fa5cc283
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-03-10 09:17:26 -0600

    updated code

--- src/main/java/com/codeup/kidsrewardscapstone/controllers/RewardController.java
+++ src/main/java/com/codeup/kidsrewardscapstone/controllers/RewardController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.codeup.kidsrewardscapstone.repositories.RewardRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RewardController {
