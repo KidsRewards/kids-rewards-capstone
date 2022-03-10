0d0200ecfe657baf101fc3dc07ac41ed7d968bdb
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-03-10 10:26:38 -0600

    Revert "Added the WishItem Controller"

--- src/main/java/com/codeup/kidsrewardscapstone/repositories/WishItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codeup.kidsrewardscapstone.repositories;
-
-import com.codeup.kidsrewardscapstone.models.Reward;
-import com.codeup.kidsrewardscapstone.models.WishItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WishItemRepository extends JpaRepository<WishItem, Long> {
-}
