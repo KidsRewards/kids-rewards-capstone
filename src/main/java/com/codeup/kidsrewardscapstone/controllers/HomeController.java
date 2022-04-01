831a94ffa32954dfb0803499f8b16bb0a8103bb5
Nicholas Diaz <nicholasdiazjr509@gmail.com>
2022-04-01 11:13:27 -0500

    still needs pull

--- src/main/java/com/codeup/kidsrewardscapstone/controllers/HomeController.java
+++ src/main/java/com/codeup/kidsrewardscapstone/controllers/HomeController.java
@@ -1,19 +1,33 @@
 package com.codeup.kidsrewardscapstone.controllers;
 
 import com.codeup.kidsrewardscapstone.models.User;
+import com.codeup.kidsrewardscapstone.repositories.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    private UserRepository usersDao;
+
+    public HomeController(UserRepository usersDAO) {
+        this.usersDao = usersDAO;
+    }
+
     @GetMapping("/")
     public String homePage(){
         return "homepage";
     }
 
     @GetMapping("/about-us")
-    public String showAboutUs() {
-        return "about-us";
+    public String showAboutUs(Model model) {
+        if(!SecurityContextHolder.getContext().getAuthentication().getPrincipal().equals("anonymousUser")) {
+            User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            model.addAttribute("loggedInUser", usersDao.getById(loggedInUser.getId()));
+            return "about-us";
+        }else{
+            return "about-us-two";
+        }
     }
 }
